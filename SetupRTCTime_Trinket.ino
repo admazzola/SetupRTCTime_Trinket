f85759d67d91bf88ba3caace460983f5d4a3b47b
root <root@andy-A770E3.(none)>
2015-01-27 19:20:35 -0500

    added debugging

--- SetupRTCTime_Trinket.ino
+++ SetupRTCTime_Trinket.ino
@@ -10,10 +10,14 @@
 
 const unsigned long DOWNLOAD_TIME = 1422315564; //time when doing the download..unix time since epoch
 
+int ledPin = 1;
 
 void setup()  {
   
- 
+  setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  
+  
+ pinMode(ledPin, OUTPUT);      // sets the digital pin as output
  
  /* if (timeStatus() != timeSet) 
      Serial.println("Unable to sync with the RTC");
@@ -30,32 +34,36 @@ void loop()
       RTC.set(t);   // set the RTC and the system time to the received value
       setTime(t);          
     
-  
-  digitalClockDisplay();  
+    
+    if(timeStatus() == timeSet){
+    debugBlink(1);//RTC got time, sent it back
+    }
+    
+    if(timeStatus() == timeNotSet){
+    debugBlink(3);//issue, RTC didnt get time
+    }
+    
+    if(timeStatus() == timeNeedsSync){
+    debugBlink(5);//issue, RTC didnt send time back to us
+    }
+    
+    
+    
   delay(1000);
 }
 
-void digitalClockDisplay(){
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
+
+
+void debugBlink(int count)
+{
+  int i=0;
+  for(i=0;i<count;i++)
+  {
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(200);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(200); 
+  }
+  
+  delay(4000);
 }
-
-void printDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
-
-
-
